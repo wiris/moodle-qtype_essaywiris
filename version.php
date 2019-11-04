fed18ff3a0f313e45e844dc07ffbd3567e59cf9c
Manuel <manuel@wiris.com>
2019-11-04 15:54:19 +0100

    Fix release number

--- version.php
+++ version.php
@@ -17,7 +17,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version = 2019103000;
-$plugin->release = '3.77.1';
+$plugin->release = '3.77.2';
 $plugin->requires = 2011060313;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->component = 'qtype_essaywiris';
